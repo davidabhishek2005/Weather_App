02103cb14f726d7ac8652e1890cda2ec8722d6f2
davidabhishek2005 <davidabhisekhg@gmail.com>
2024-01-20 22:14:15 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import WeatherApp from './components/Weather App/WeatherApp';
+
+function App() {
+  return (
+    <div className="App">
+       <WeatherApp/>
+      
+    </div>
+  );
+}
+
+export default App;
